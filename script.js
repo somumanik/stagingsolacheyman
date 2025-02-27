e0e195aa27341ac4dabb3b7d68ce27ef51a1d03b
somumanik <manikchowdhuri@gmail.com>
2025-02-27 20:37:32 +0530

    changes

---
+++ script.js
@@ -0,0 +1,18 @@
+let showMegaMenu = document.querySelector(".header_mid_right");
+let midBar = document.querySelector('.mid-bar');
+let closeButton =  document.querySelector('.header_mid_right_nav nav  .closebutton');
+let showCloseMenu = document.querySelector('.megmenu')
+
+
+
+
+midBar.addEventListener('click',()=>
+{
+    showMegaMenu.classList.add("showBar")
+})
+
+closeButton.addEventListener('click',()=>
+{
+    // alert("ok")
+    showCloseMenu.classList.add('closeMenu')
+})
